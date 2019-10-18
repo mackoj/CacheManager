64963317d5929eb434ecb3ad59e54e2fcc549799
Jeffrey Macko <github.jm@macko.fr>
2019-10-18 22:02:39 +0200

    Correction

--- Package.swift
+++ Package.swift
@@ -24,6 +24,6 @@ let package = Package(
         .testTarget(
             name: "CacheManagerTests",
             dependencies: ["CacheManager"]),
-    ]
+    ],
     swiftLanguageVersions: [.v5]
 )
